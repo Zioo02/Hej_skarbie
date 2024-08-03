c2732153f89e6cad0a4ac9f330571c05b29ccdfb
Zioo02 <kadzielat@gmail.com>
2024-08-03 22:33:19 +0200

    final

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('actionButton');
-    button.addEventListener('click', () => {
-        alert('Button clicked!');
-    });
-});
